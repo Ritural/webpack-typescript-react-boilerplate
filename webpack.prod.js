250d01cdd12e1338050d8649679a111ae9dbcd9a
Kallan Jones <kallan.jones@gmail.com>
2019-09-18 20:00:53 +1200

    Add source-maps to production.

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'source-map', // Sets up sourcemaps to be used in the browser
 
   plugins: [new MinifyPlugin()]
 });
